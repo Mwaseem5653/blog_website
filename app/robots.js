f7bf699996bc1fba381bfce9ebdcf773185792a6
MuhammadWaseem5196 <gamingrakshas1947@gmail.com>
2025-11-26 12:10:22 +0500

    final robot

--- app/robots.js
+++ app/robots.js
@@ -1,10 +1,13 @@
 export default function robots() {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/studio',
-    },
-    sitemap: 'https://glowguideblogs.vercel.app/sitemap.xml',
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/studio"],
+      },
+    ],
+    sitemap: "https://glowguideblogs.vercel.app/sitemap.xml",
+    host: "https://glowguideblogs.vercel.app",
   };
 }
