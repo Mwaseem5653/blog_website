ffbf5c9f8c1c082874e71314263e87f8565b2b26
MuhammadWaseem5196 <gamingrakshas1947@gmail.com>
2025-12-02 17:25:14 +0500

    main

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,12 @@ const nextConfig = {
         port: "",
         pathname: "/images/**",
       },
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+        port: "",
+        pathname: "/**",
+      }
     ],
   },
 };
